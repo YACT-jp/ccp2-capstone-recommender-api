392c49da614fc16f049007505bd7bbf3c438d9d7
JeanFernandez <jean.fernandez_imt@outlook.com>
2021-12-03 00:05:24 +0900

    Create Express server and deploy

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const axios = require("axios");
 
 const app = express();
 
@@ -18,10 +19,59 @@ app.get("/", (req, res) => {
 app.get("/recommender", async (req, res) => {
   const { user_id } = req.body;
 
-  console.log(user_id);
-  let result = "Testing!";
+  retrieval_body = {
+    instances: [user_id],
+  };
 
-  res.status(200).json(result);
+  const retrieval = await axios.default.post(
+    "https://ccp2-capstone-ml-models-retrival-cr-yxiyypij7a-an.a.run.app/v1/models/retrieval:predict",
+    retrieval_body
+  );
+
+  const ranking_body = {
+    instances: [],
+  };
+
+  for (media of retrieval.data.predictions[0].output_2) {
+    ranking_body.instances.push({
+      movie_title: media,
+      user_id: user_id,
+    });
+  }
+
+  const ranking = await axios.default.post(
+    "https://ccp2-capstone-ml-models-ranking-cr-yxiyypij7a-an.a.run.app//v1/models/ranking:predict",
+    ranking_body
+  );
+
+  const media_and_ranking = [];
+
+  for (let i = 0; i < retrieval.data.predictions[0].output_2.length; i++) {
+    media_and_ranking.push({
+      media: retrieval.data.predictions[0].output_2[i],
+      ranking: ranking.data.predictions[i][0],
+    });
+  }
+
+  const compareRanking = (a, b) => {
+    if (a.ranking < b.ranking) return 1;
+    if (a.ranking > b.ranking) return -1;
+    if (a.ranking === b.ranking) return 0;
+  };
+
+  sorted_media_and_ranking = media_and_ranking.sort(compareRanking);
+
+  const results = [];
+
+  for (item of sorted_media_and_ranking) {
+    results.push(item.media);
+  }
+
+  if (results.length < 10) {
+    res.status(400).json([]);
+  } else {
+    res.status(200).json(results);
+  }
 });
 
 (() => {
